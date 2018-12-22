910ac4a606d0b822fc0019226f4d94f6b67b9304
LevshinMisha <LevshinMisha@github.com>
2018-12-22 18:51:21 +0500

    Добавим возможность дебага

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import Bot, { Message, File } from 'telegram-api';
  
 const bot = new Bot({ token: '413121960:AAE2_z-PLg2uwo_NMfcMwThjGN86QcDKE6E' });
+
+let debugChat = false;
  
 bot.start();
 
 const sendMessage = (text, to) => bot.send(new Message().text(text).to(to));
+
+const sendDebug = text => debugChat ? sendMessage(JSON.stringify(text), debugChat) : null;
+
+bot.command('start', message => {
+
+})
  
 bot.get('', message => {
-  console.log(message);
-  sendMessage('Зачем ты мне пишешь?', message.chat.id);
+  debugChat = message.chat.id;
+  sendDebug(message);
+  sendMessage(`Зачем ты мне пишешь? ${message.chat.id}`, message.chat.id);
 });
