b990f5b48e442bc05b733a7f28cb5156671f605a
LevshinMisha <LevshinMisha@github.com>
2018-12-22 23:44:31 +0500

    Стартуем, только если не в чате

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -2,16 +2,20 @@ import { sendMessage, sendMessages, sendDebug } from '../api';
 import { clearQuery, addToQuery, getQuery } from '../model/query';
 import { openChat } from '../model/chat';
 
-export default (message, chatId) => {
+export default (message, chatId, otherChatId) => {
   sendDebug('start start', message);
-  sendMessage('Начинаем поиск', chatId);
-  const queryChat = getQuery();
-  if (queryChat) {
-    sendMessages('Поиск успешно завершен', [queryChat, chatId]);
-    sendMessages('Можете начинать общение', [queryChat, chatId]);
-    openChat(queryChat, chatId);
-    clearQuery();
-  } else
-    addToQuery(chatId)
+  if (!otherChatId) {
+    sendMessage('Начинаем поиск', chatId);
+    const queryChat = getQuery();
+    if (queryChat) {
+      sendMessages('Поиск успешно завершен', [queryChat, chatId]);
+      sendMessages('Можете начинать общение', [queryChat, chatId]);
+      openChat(queryChat, chatId);
+      clearQuery();
+    } else
+      addToQuery(chatId);
+  } else 
+    sendMessage('Для начала закончи текущую беседу', chatId);
+  
   sendDebug('start end', message);
 }
