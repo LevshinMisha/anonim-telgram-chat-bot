16f27851dbba0783ee07b43ceae547986643ca74
LevshinMisha <LevshinMisha@github.com>
2018-12-26 11:41:41 +0500

    Вынес абстракцию бота

---
+++ src/bot.js
@@ -0,0 +1,23 @@
+import initApi from './telegram';
+import { getOtherUser } from './model/chat';
+
+const setHandler = handler => message => {
+  const chatId = message.chat.id;
+  const otherChatId = getOtherUser(chatId);
+  const { text } = message;
+  return handler(message, chatId, otherChatId, text);
+};
+
+export default class Bot {
+  constructor(token) {
+    this.bot = initApi(token);
+  }
+
+  onCommand(command, handler) {
+    this.bot.command(command, setHandler(handler));
+  }
+
+  onMessage(pattern, handler) {
+    this.bot.get(pattern, setHandler(handler));
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-import initApi from './telegram';
-import { getOtherUser } from './model/chat';
+import Bot from './bot';
 import messageHandler from './handlers/message';
 import startHandler from './handlers/start';
 import finishHandler from './handlers/finish';
 import debugHandler from './handlers/debug';
 
-const bot = initApi('413121960:AAE2_z-PLg2uwo_NMfcMwThjGN86QcDKE6E');
+const bot = new Bot('413121960:AAE2_z-PLg2uwo_NMfcMwThjGN86QcDKE6E');
 
-const setHandler = handler => message => {
-  const chatId = message.chat.id;
-  const otherChatId = getOtherUser(chatId);
-  const { text } = message;
-  return handler(message, chatId, otherChatId, text);
-};
-
-bot.command('start', setHandler(startHandler));
-bot.command('finish', setHandler(finishHandler));
-bot.command('debug', setHandler(debugHandler));
-bot.get('', setHandler(messageHandler));
+bot.onCommand('start', startHandler);
+bot.onCommand('finish', finishHandler);
+bot.onCommand('debug', debugHandler);
+bot.onMessage('', messageHandler);
 
 require('http').createServer((req, res) => {
   res.write(req.url === '/ping' ? 'pong' : 'Hello World!');
